f2fa871992d013576370901bc7b71223e50e0f53
Maxsmile123 <sysoevmaksim27@gmail.com>
2023-08-20 02:39:54 +0300

    yo

--- Cmake/BuildFlags.cmake
+++ Cmake/BuildFlags.cmake
@@ -1,19 +1,14 @@
-if (CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 9.3)
-  set(CMAKE_CXX_STANDARD 17)
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 10.0)
-  set(CMAKE_CXX_STANDARD 17)
-else()
-  set(CMAKE_CXX_STANDARD 17)
-endif()
+set(CMAKE_CXX_STANDARD             20)
+set(CMAKE_C_STANDARD             11)
 
-if (CMAKE_COMPILER_IS_GNUCC OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -g")
-else()
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -g")
-endif()
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic -g")
+set(GCC_COMPILE_FLAGS "${GCC_COMPILE_FLAGS} -Wall -Wextra -Wpedantic -g")
 
-set(CMAKE_CXX_FLAGS_ASAN "-g -fsanitize=address,undefined -fno-sanitize-recover=all"
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address,undefined -fno-sanitize-recover=all"
     CACHE STRING "Compiler flags in asan build"
     FORCE)
 
-set(CMAKE_CXX_FLAGS_COVERAGE "${CMAKE_CXX_FLAGS_ASAN} -fprofile-instr-generate -fcoverage-mapping")
+set(GCC_COMPILE_FLAGS "${GCC_COMPILE_FLAGS} -fsanitize=address,undefined -fno-sanitize-recover=all"
+    CACHE STRING "Compiler flags in asan build"
+    FORCE)
+

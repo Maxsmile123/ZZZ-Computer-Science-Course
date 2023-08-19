95c7fd3e6f2d6133017a725bdf8789ca8de0668c
Maxsmile123 <sysoevmaksim27@gmail.com>
2023-08-19 23:49:08 +0300

    t

---
+++ tasks/geometry/tasks/13/solution.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+
+typedef struct point_{
+    int x;
+    int y;
+} Point;
+
+int check_zone(Point p){
+    // Write a code here that checks whether a point belongs to a given area
+}
+
+int foo(){
+    // write main code here
+}

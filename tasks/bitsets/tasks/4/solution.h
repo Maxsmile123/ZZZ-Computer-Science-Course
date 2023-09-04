853d51f79fdc1478189b7b36320e512d3aec3e81
Maxsmile123 <sysoevmaksim27@gmail.com>
2023-09-04 21:45:01 +0300

    мпммр

--- tasks/bitsets/tasks/4/solution.h
+++ tasks/bitsets/tasks/4/solution.h
@@ -1,23 +1,11 @@
 #pragma once
 
-#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 typedef uint32_t Bitset;
 
-const int SIZE = 10;
-
-int Hah() {
-    int* a = (int*)malloc(SIZE);
-    free(a);
-    return printf("Hello, man!\n");
-}
-
 int Task() {
     // write your solution here
-    Hah();
-    Bitset set = 0;
-    return set;
+    return 0;
 }

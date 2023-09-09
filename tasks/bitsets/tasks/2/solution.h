b57b108e1940cc85f3063560dbfee55ccfd75d42
Maxsmile123 <sysoevmaksim27@gmail.com>
2023-09-09 11:19:18 +0300

    lol

--- tasks/bitsets/tasks/2/solution.h
+++ tasks/bitsets/tasks/2/solution.h
@@ -3,9 +3,9 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef uint32_t Bitset; 
+typedef uint32_t Bitset;
 
 int Task() {
-    // write your solution here
+    // write your solution here.
     return 0;
 }

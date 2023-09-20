4e2b2d5442c0635859d15d7532e13b9d364817cc
German2104 <gper9020@gmail.com>
2023-09-20 15:33:03 +0300

    Permyakov

--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,1 +1,5 @@
 # Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
+find. -maxdepth -name "*py" -name "*keylogger*"
+find. -maxdepth -name "*py" -name "*trojan*"
+find. -maxdepth -name "*py" -name "*backdoor*" 
+

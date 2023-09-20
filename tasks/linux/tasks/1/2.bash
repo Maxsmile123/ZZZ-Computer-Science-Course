1683907678f6488553b7f288e586c44769f970a5
freymi1856 <maksimpavlusin6@gmail.com>
2023-09-20 17:38:12 +0300

    laba linux

--- tasks/linux/tasks/1/2.bash
+++ tasks/linux/tasks/1/2.bash
@@ -1,2 +1,2 @@
-# Объедините два текстовых файла file1.txt и file2.txt в третий - file3.txt
-
+# Объедините два текстовых файла file1.txt и file2.txt в третий - file3.txt	
+cat text1.txt text2.txt > text3.txt

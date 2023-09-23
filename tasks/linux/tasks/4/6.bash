1d31a7734e6a1fd868cb8409ff41142e0cfd561f
German2104 <gper9020@gmail.com>
2023-09-23 11:51:01 +0300

    lab1

--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,3 +1,3 @@
 # Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger.
-find. -maxdepth -name "*py" -name "*keylogger*" | -name "trojan" | -name "backdoor" 
+find /-type f  -name "*py"-a name "*keylogger*" -a  name "*trojan*" -a name "*backdoor*" 
 

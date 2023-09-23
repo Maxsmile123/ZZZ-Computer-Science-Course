883a7b00ce30b3938db73467d29b99f9b48856bf
dontknovwv <dont.knovvvvvv@gmail.com>
2023-09-23 23:43:46 +0300

    Task.6

--- tasks/linux/tasks/4/6.bash
+++ tasks/linux/tasks/4/6.bash
@@ -1,2 +1,2 @@
  Найдите все py файлы, содержащие в названии любое из трёх слов: backdoor, trojan, keylogger
- find . -name "*.py" -name "*backdoor*" -o -name "*.py" -name "*trojan*" -o -name "*pu" -name "*keylogger*" 
+ find . -name "*.py" -name "*backdoor*" -o -name "*.py" -name "*trojan*" -o -name "*.pu" -name "*keylogger*"

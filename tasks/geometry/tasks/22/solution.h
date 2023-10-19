a21276c1d02969ed2842183e86f3266bafba612e
DmkitryNai <naipakdmitry@yandex.ru>
2023-10-19 23:51:30 +0300

    Dmitry Naypak - Geometry-Laba5

--- tasks/geometry/tasks/22/solution.h
+++ tasks/geometry/tasks/22/solution.h
@@ -1,6 +1,56 @@
-#pragma once
-
 #include <stdio.h>
+
+const int MINUS_TEN = -10;
+const int TEN = 10;
+const int TWENTY = 20;
+const int INITIAL_I = 8;
+const int INITIAL_J = 15;
+const int INITIAL_L = 10;
+const int Y_0 = 10;
+const int X_0 = -10;
+const int ITERATIONS = 50;
+const int ONE = 1;
+const int MINUS_ONE = -1;
+const int THITRY = 30;
+const int TWO = 2;
+
+int Abs(int number_1) {
+    return (number_1 >= 0) ? number_1 : -number_1;
+}
+
+// Function return mod of numbers
+int Mod(int number_1, int number_2) {
+    if (number_2 != 0) {
+        return (Abs(number_1 % number_2));
+    } else {
+        return 0;
+    }
+}
+
+// Function of return sign of numbers
+int Sign(int number_1) {
+    if (number_1 > 0) {
+        return 1;
+    } else if (number_1 < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+// Function for maximum of numbers
+int Max(int number_1, int number_2) {
+    return (number_1 >= number_2) ? number_1 : number_2;
+}
+
+// Function minimen of numbers
+int Min(int number_1, int number_2) {
+    return (number_1 >= number_2) ? number_2 : number_1;
+}
+
+int Module(int number_1) {
+    return (number_1 >= 0) ? number_1 : number_1 * MINUS_ONE;
+}
 
 typedef struct {
     int x;
@@ -8,11 +58,33 @@ typedef struct {
 } Point;
 
 int CheckZone(Point p) {
-    // Write a code here that checks whether a point belongs to a given area
-    return 0;
+    return ((p.x + Y_0 <= p.y) && ((MINUS_ONE * p.x) + Y_0 >= p.y) && (p.x >= X_0));
 }
 
 int Task() {
-    // write main code here
+    int i = INITIAL_I;
+    int j = INITIAL_J;
+    int l = INITIAL_L;
+    Point p;
+    p.x = i;
+    p.y = j;
+    int in_zone_counter = 0;
+    for (int k = ONE; k <= ITERATIONS; ++k) {
+        i = Mod(Mod((p.x + p.y), ((Module(Min(p.y - l, l - k)) + ONE) - k)), TWENTY) + TEN;
+        j = Max((p.x + p.y) / (TWO + Sign((p.y * l) - (p.x * k))), (p.y + l) / (TWO + Sign((p.x * p.y) - (l * k)))) -
+            TEN;
+        l = Mod(Max(p.x, p.y) * Min(p.x, l), THITRY);
+        p.x = i;
+        p.y = j;
+        if (CheckZone(p)) {
+            ++in_zone_counter;
+        }
+        printf("k = %d, x = %d, y = %d, l = %d passed = %d \n", k, p.x, p.y, l, CheckZone(p));
+    }
+    if (in_zone_counter == 0) {
+        printf("The point did not fall into the area beyond 50 iterations\n");
+    } else {
+        printf("Number of points belonging to the zone: %d\n", in_zone_counter);
+    }
     return 0;
 }

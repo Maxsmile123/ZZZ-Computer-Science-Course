b70fba3dcd8653c66603985ab46d65be2298f6be
Dmitrii Bozhok <143950862+canadec2@users.noreply.github.com>
2023-10-21 00:17:55 +0300

    GEOMETRY_3 BOZHOK

--- tasks/geometry/tasks/3/solution.h
+++ tasks/geometry/tasks/3/solution.h
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 const int MAX_ITERATIONS = 50;
-const int cord_X1 = 10;
-const int cord_Y1 = 10;
-const int rad_1 = 5;
-const int rad_2 = 10;
+const int CORD_X1 = 10;
+const int CORD_Y1 = 10;
+const int RAD_1 = 5;
+const int RAD_2 = 10;
+const int I = 0;
+const int J = 0;
+const int K = 0;
+const int L = 0;
+const int IK = 1;
+const int JK = -30;
+const int LK = 1;
+const int B = 0;
 
 typedef struct {
     int x;
@@ -14,23 +22,23 @@ typedef struct {
 int CheckZone(Point p) {
     int cord_x = p.x;
     int cord_y = p.y;
-    int first_x = (cord_x - cord_X1) * (cord_x - cord_X1);
-    int first_y = (cord_y - cord_Y1) * (cord_y - cord_Y1);
-    int rad_square_1 = rad_1 * rad_1;
-    int rad_square_2 = rad_2 * rad_2;
+    int first_x = (cord_x - CORD_X1) * (cord_x - CORD_X1);
+    int first_y = (cord_y - CORD_Y1) * (cord_y - CORD_Y1);
+    int rad_square_1 = RAD_1 * RAD_1;
+    int rad_square_2 = RAD_2 * RAD_2;
     if ((first_x + first_y >= rad_square_1) && (first_x + first_y <= rad_square_2)) {
-	    return 1;
+	return 1;
     } else {
-	    return 2;
+	return 2;
     }
 }
 
 int mod(int x, int y) {
     if (y != 0) {
 	return x % y;
-} else {
+    } else {
 	return 0;
-}
+    }
 }
 int abs(int x) {
     if (x > 0) {
@@ -68,17 +76,9 @@ int minz(int x, int y) {
 }
 
 int Task() {
-    const int i = 0;
-    const int j = 0;
-    const int k = 0;
-    const int l = 0;
-    const int ik = 1;
-    const int jk = -30;
-    const int lk = 1;
-    const int b = 0;
-    int i_0 = ik;
-    int j_0 = jk;
-    int l_0 = lk;
+    int i_0 = IK;
+    int j_0 = JK;
+    int l_0 = LK;
     for (int i = 0; i < MAX_ITERATIONS; ++i) {
 	Point p;
 	p.x = i_0;
@@ -88,12 +88,12 @@ int Task() {
 	if (CheckZone(p) == 1) {
 		printf("x = %d, y = %d\n", i_0, j_0);
 	}
-	i_1 = i_0 / ik - (i_0 - i) * sign(l_0 - j_0);
-	j_1 = mod(j_0, lk) - mod(maxz(i_0, l_0), 10) % (i + 1);
-	int l_1 = i_0 + mod(j_0 * i, ik) + l_0 / jk + ik;
+	i_1 = i_0 / IK - (i_0 - i) * sign(l_0 - j_0);
+	j_1 = mod(j_0, LK) - mod(maxz(i_0, l_0), 10) % (i + 1);
+	int l_1 = i_0 + mod(j_0 * i, IK) + l_0 / JK + IK;
 	i_0 = i_1;
 	j_0 = j_1;
 	l_0 = l_1;
     }
-    return 0;
+   return 0;
 }

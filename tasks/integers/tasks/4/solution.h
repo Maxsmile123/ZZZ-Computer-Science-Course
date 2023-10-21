18d2bb2472092c1e557b64e733fe756a0d98ec46
sergey <usov.serzh.04@mail.ru>
2023-10-21 13:30:23 -0400

    int

--- tasks/integers/tasks/4/solution.h
+++ tasks/integers/tasks/4/solution.h
@@ -4,6 +4,24 @@
 #include <stdio.h>
 
 int Task() {
-    // write your solution here
+    uint64_t number = 0;
+    scanf("%lu", &number);
+    uint8_t first_ch = 0;
+    uint8_t second_ch = 0;
+    uint8_t third_ch = 0;
+    const uint8_t num_10 = 10;
+    if (number < num_10 * num_10) {
+        return 0;
+    }
+    while (number > 0) {
+        if ((number % num_10) > ((number / num_10) % num_10) &&
+            ((number / num_10) % num_10 > (number / num_10 * num_10) % num_10)) {
+            first_ch = number % num_10;
+            second_ch = (number / num_10) % num_10;
+            third_ch = (number / num_10 * num_10) % num_10;
+            printf("%u%u%u\n", third_ch, second_ch, first_ch);
+        }
+        number /= num_10;
+    }
     return 0;
 }

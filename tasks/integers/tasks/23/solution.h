c3a28bce0fb930df116307e9632940d8cbf2ca8a
German2104 <139492070+German2104@users.noreply.github.com>
2023-10-22 19:15:03 +0300

    Integeres_23.h

--- tasks/integers/tasks/23/solution.h
+++ tasks/integers/tasks/23/solution.h
@@ -5,5 +5,13 @@
 
 int Task() {
     // write your solution here
+    int count_number = 0;
+    char c;
+    while ((c = getchar()) != EOF) {
+        count_number += 1;
+        if ((count_number % 2) != 0) {
+            printf("%c", c);
+        }
+    }
     return 0;
 }

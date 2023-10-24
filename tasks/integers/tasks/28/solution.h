ba220eecfde8caae57723ce39a36f6629fd39afc
gleb <bleb_1@mail.ru>
2023-10-24 15:45:45 +0300

    int

--- tasks/integers/tasks/28/solution.h
+++ tasks/integers/tasks/28/solution.h
@@ -5,5 +5,19 @@
 
 int Task() {
     // write your solution here
+    int32_t number_in_10;
+    int64_t number_in_2;
+    int64_t multyplier = 1;
+    scanf("%d", &number_in_10);
+    if (number_in_10 < 0) {
+        number_in_10 *= -1;
+        printf("-");
+    }
+    while (number_in_10 > 0) {
+        number_in_2 += (number_in_10 % 2) * multyplier;
+        number_in_10 /= 2;
+        multyplier *= 10;
+    }
+    printf("%ld", number_in_2);
     return 0;
 }

d4addf4880b354f22ea38657f59904d2c68d3f99
gleb <bleb_1@mail.ru>
2023-10-26 15:22:04 +0300

    int3

--- tasks/integers/tasks/28/solution.h
+++ tasks/integers/tasks/28/solution.h
@@ -5,19 +5,19 @@ const int TEN = 10;
 
 int Task() {
     // write your solution here
-    int32_t number_in_10 = 0;
-    int64_t number_in_2 = 0;
+    int32_t number_in_10d = 0;
+    int64_t number_in_2d = 0;
     int64_t multyplier = 1;
-    scanf("%d", &number_in_10);
-    if (number_in_10 < 0) {
-        number_in_10 *= -1;
+    scanf("%d", &number_in_10d);
+    if (number_in_10d < 0) {
+        number_in_10d *= -1;
         printf("-");
     }
-    while (number_in_10 > 0) {
-        number_in_2 += (number_in_10 % 2) * multyplier;
-        number_in_10 /= 2;
+    while (number_in_10d > 0) {
+        number_in_2d += (number_in_10d % 2) * multyplier;
+        number_in_10d /= 2;
         multyplier *= TEN;
     }
-    printf("%ld\n", number_in_2);
+    printf("%ld\n", number_in_2d);
     return 0;
 }

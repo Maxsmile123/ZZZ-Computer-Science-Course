7943ae29feefcef0c1794867b25afa89bc0ccc6b
sergey <usov.serzh.04@mail.ru>
2023-10-28 07:51:33 -0400

    z

--- tasks/integers/tasks/4/solution.h
+++ tasks/integers/tasks/4/solution.h
@@ -3,25 +3,37 @@
 #include <stdint.h>
 #include <stdio.h>
 
+const uint8_t NUM_10 = 10;
+
+uint8_t ThirdDigit(uint64_t number) {
+    return (number / (NUM_10 * NUM_10)) % NUM_10;
+}
+
+uint8_t SecondDigit(uint64_t number) {
+    return (number / NUM_10) % NUM_10;
+}
+
+uint8_t FirstDigit(uint64_t number) {
+    return number % NUM_10;
+}
+
 int Task() {
     uint64_t number = 0;
     scanf("%lu", &number);
-    uint8_t first_ch = 0;
-    uint8_t second_ch = 0;
-    uint8_t third_ch = 0;
-    const uint8_t num_10 = 10;
-    if (number < num_10 * num_10) {
+    uint8_t first_num = 0;
+    uint8_t second_num = 0;
+    uint8_t third_num = 0;
+    if (number < NUM_10 * NUM_10) {
         return 0;
     }
     while (number > 0) {
-        if ((number % num_10) > ((number / num_10) % num_10) &&
-            ((number / num_10) % num_10 > (number / num_10 * num_10) % num_10)) {
-            first_ch = number % num_10;
-            second_ch = (number / num_10) % num_10;
-            third_ch = (number / num_10 * num_10) % num_10;
-            printf("%u%u%u\n", third_ch, second_ch, first_ch);
+        first_num = FirstDigit(number);
+        second_num = SecondDigit(number);
+        third_num = ThirdDigit(number);
+        if (first_num > second_num && second_num > third_num) {
+            printf("%u%u%u\n", third_num, second_num, first_num);
         }
-        number /= num_10;
+        number /= NUM_10;
     }
     return 0;
 }

532dc26660b05ef48022c8d4e0ef2d48105b6c68
sergey <usov.serzh.04@mail.ru>
2023-11-01 13:24:03 -0400

    .

--- tasks/bitsets/tasks/13/solution.h
+++ tasks/bitsets/tasks/13/solution.h
@@ -1,11 +1,35 @@
 #pragma once
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
 typedef uint32_t Bitset;
 
 int Task() {
-    // write your solution here
+    char c = 0;
+    Bitset bitset = 0;
+    while (true) {
+        c = (char)getchar();
+        if (c == EOF) {
+            break;
+        }
+        if (c >= 'a' || c == 'e' || c == 'o' || c == 'i' || c == 'u' || c == 'y') {
+            bitset = bitset | (1u << (c - 'a'));
+        }
+    }
+    const uint8_t max_glasn = 6;
+    const uint8_t lens = 26;
+    uint8_t count = 0;
+    for (int i = lens; i >= 0; --i) {
+        if ((bitset >> i) & 1) {
+            ++count;
+        }
+    }
+    if (count < max_glasn) {
+        printf("yes\n");
+    } else {
+        printf("no\n");
+    }
     return 0;
 }

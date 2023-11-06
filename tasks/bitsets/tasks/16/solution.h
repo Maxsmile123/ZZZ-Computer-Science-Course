3e8abca8ee7a37e2e90a8b943f71a74d935b3037
German2104 <139492070+German2104@users.noreply.github.com>
2023-11-06 22:10:22 +0300

    Update solution.h

--- tasks/bitsets/tasks/16/solution.h
+++ tasks/bitsets/tasks/16/solution.h
@@ -10,6 +10,7 @@ int Task() {
     char c;
     Bitset bitset = 0;
     int count_glas_1 = 0;
+    int count_glas_2 = 0;
     while (true) {
         c = (char)getchar();
         if (c == EOF) {
@@ -17,15 +18,15 @@ int Task() {
         }
         if (c == 'a' || c == 'e' || c == 'o' || c == 'i' || c == 'u' || c == 'y') {
             ++count_glas_1;
-        }
-        if (c == " ") {
-            int count_glas_2 = count_glas_1;
-            count_glas_1 = 0;
         if (count_glas_1 == count_glas_2) {
             printf("YES");
         } else {
             printf("NO");
+            }
         }
+        if (c == " " && count_glas_1 != 0) {
+            count_glas_2 = count_glas_1;
+            count_glas_1 = 0;
         }
     }
     return 0;

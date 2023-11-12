44608a110a427d52e440f21a0255a1c0d2ebc40d
sergey162 <usov.serzh.04@mail.ru>
2023-11-12 15:14:33 -0500

    bitset

--- tasks/bitsets/tasks/13/solution.h
+++ tasks/bitsets/tasks/13/solution.h
@@ -1,18 +1,29 @@
+#pragma once
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
-void Checker(uint8_t count, uint8_t max_glasn) {
-    if (count < max_glasn) {
-        printf("yes\n");
+typedef uint32_t Bitset;
+
+void CheckVowelFreedom(Bitset bitset) {
+    const uint8_t max_vowels = 6;
+    const uint8_t len_alphabet = 26;
+    uint8_t count = 0;
+    for (int i = len_alphabet; i >= 0; --i) {
+        if ((bitset >> i) & 1) {
+            ++count;
+        }
+    }
+    if (count < max_vowels) {
+        printf("\n\nYes\n");
     } else {
-        printf("no\n");
+        printf("\n\nNo\n");
     }
 }
 
-typedef uint32_t Bitset;
-
 int Task() {
+    const Bitset all_vowels = 17842449;
     char c = 0;
     Bitset bitset = 0;
     while (true) {
@@ -20,18 +31,10 @@ int Task() {
         if (c == EOF) {
             break;
         }
-        if (c == 'a' || c == 'e' || c == 'o' || c == 'i' || c == 'u' || c == 'y') {
+        if ((all_vowels >> (c - 'a')) & 1) {
             bitset = bitset | (1u << (c - 'a'));
         }
     }
-    const uint8_t max_glasn = 6;
-    const uint8_t lens = 26;
-    uint8_t count = 0;
-    for (int i = lens; i >= 0; --i) {
-        if ((bitset >> i) & 1) {
-            ++count;
-        }
-    }
-    Checker(count, max_glasn);
+    CheckVowelFreedom(bitset);
     return 0;
 }

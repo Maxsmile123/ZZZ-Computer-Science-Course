33592a1331fc10ed0b566ffa07fc6a4a2579f942
Zlata <zlatachernyshkova1999@gmail.com>
2023-12-09 18:41:40 +0300

    geometry1

--- tasks/geometry/tasks/1/solution.h
+++ tasks/geometry/tasks/1/solution.h
@@ -1,6 +1,17 @@
 #pragma once
-
 #include <stdio.h>
+
+const int MAX_ITERATIONS = 50;
+const int I_0 = 18;
+const int J_0 = -9;
+const int L_0 = 5;
+const int X_0 = 10;
+const int Y_0 = 10;
+const int R_BIG = 10;
+const int R_SMALL = 5;
+const int THIRTY = 30;
+const int TWENTY = 20;
+const int TEN = 10;
 
 typedef struct {
     int x;
@@ -8,11 +19,84 @@ typedef struct {
 } Point;
 
 int CheckZone(Point p) {
-    // Write a code here that checks whether a point belongs to a given area
-    return 0;
+    
+    return ((((p.x - X_0) * (p.x - X_0) + (p.y - Y_0) * (p.y - Y_0) <= R_BIG * R_BIG) &&
+            ((p.x - X_0) * (p.x - X_0) + (p.y - Y_0) * (p.y - Y_0) >= R_SMALL * R_SMALL)));
+    }
+
+
+int Max(int x, int y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int Mod(int x, int y) {
+    if (y != 0) {
+        return x % y;
+    } else {
+        return 0;
+    }
+}
+
+
+
+int Min(int x, int y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+int Sign(int x) {
+    if (x > 0) {
+        return 1;
+    }
+    if (x < 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+int Abs(int x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return x * (-1);
+    }
 }
 
 int Task() {
-    // write main code here
+    int i = I_0;
+    int j = J_0;
+    int l = L_0;
+    Point p;
+    p.x = i;
+    p.y = j;
+    int flag = 0;
+
+
+    for (int k = 1; k <= MAX_ITERATIONS; ++k) {
+        if (CheckZone(p)){
+            flag = 1;
+            printf("The point with the coordinates '%d', '%d' hit into the zone", p.x, p.y);
+            break;
+        }
+
+        i = Mod(p.x * Max(p.x, l), THIRTY) + Mod(p.y * Min(p.x, l), TWENTY) + k;
+        j = Min(p.x, Max(p.y, Min(l, Max(p.x - l, p.y - l))));
+        l = Sign(k - 10) * Abs(p.x - p.y + l - k);
+
+    }
+
+
+    if (flag == 0) {
+        printf("The point did not hit the area in 50 iterations\n");
+    }
+
     return 0;
 }

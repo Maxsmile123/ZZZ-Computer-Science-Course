da055ed30c14f053cf0a127ad69936b1566e6c77
Dmitrii Bozhok <143950862+canadec2@users.noreply.github.com>
2023-12-24 11:39:36 +0300

    Delete tasks/linux/tasks/3/10.bash

--- tasks/linux/tasks/3/10.bash
+++
@@ -1,7 +0,0 @@
-'''
-Найдите все адреса эл.почты в домене gmail.com в файле personal.txt. Напомню, как выглядят адреса:
-hey.uy@gmail.com
-NiceTask@gmail.com
-'''
-cat personal.txt | grep ".@gmail.com"
-

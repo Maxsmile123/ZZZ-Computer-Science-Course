649d839d48c39353bb8c0c478e728dbd3ab9835e
e1n4ach <71188909+e1n4ach@users.noreply.github.com>
2024-01-04 19:10:47 +0300

    inter_21_3

--- tasks/integers/tasks/21/solution.h
+++ tasks/integers/tasks/21/solution.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 
+int32_t Findlen(int64_t num) {
+    int32_t counter = 0 while (num > 0) {
+        num /= 10;
+        counter += 1;
+    }
+    return counter;
+}
+
 int Task() {
     // write your solution here
-    return 0;
+    int64_t num = 333;
+    int32_t c = Findlen(num);
+    num = pow(10, c) + num;
+    num = num * 10 + 1;
+    printf("%ld", num);;
 }

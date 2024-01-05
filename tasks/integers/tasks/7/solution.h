9ec92aae0b8071fc0bdbeaab2dfd9998e7f989f6
Zlata <zlatachernyshkova1999@gmail.com>
2024-01-06 02:55:20 +0300

    update int

--- tasks/integers/tasks/7/solution.h
+++ tasks/integers/tasks/7/solution.h
@@ -3,7 +3,32 @@
 #include <stdint.h>
 #include <stdio.h>
 
+int64_t Sign(int64_t x) {
+    if (x > 0) {
+        return 1;
+    } else if (x < 0) {
+        return -1;
+    }
+}
+
 int Task() {
-    // write your solution here
+    int64_t num;
+    while (scanf("%ld", &num) == 1) {
+        if (Sign(num) == 1) {
+            printf("%ld\n", num);
+            break;
+        } else {
+            num *= -1;
+        }
+        int64_t multiple = 1;
+        int64_t new_num = 0;
+        while (num != 0) {
+            new_num += (9 - (num % 10)) * multiple;
+            multiple *= 10;
+            num /= 10;
+        }
+        printf("%ld\n", new_num);
+        break;
+    }
     return 0;
 }

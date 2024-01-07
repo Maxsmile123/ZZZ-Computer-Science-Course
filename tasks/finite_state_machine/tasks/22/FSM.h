94ca7672bc437c968b27d0eb90d05e301bf3c956
German2104 <139492070+German2104@users.noreply.github.com>
2024-01-07 20:31:17 +0300

    Update FSM.h

--- tasks/finite_state_machine/tasks/22/FSM.h
+++ tasks/finite_state_machine/tasks/22/FSM.h
@@ -2,13 +2,13 @@
 
 #include <stdio.h>
 
-typedef enum states {
-    S0,
-    S1
-    // struct with states for your KA
-} State;
+#include "fsm_structs.h"
+#include "fsm_transition.h"
 
 int Task() {
-    // write your solution here
+    FILE *file = fopen("../tasks/finite_state_machine/tasks/22/test.txt", "r");
+    Machine my_machine;
+    my_machine.current_state = SCAN;
+    Transition(&my_machine, file);
     return 0;
 }

8de34eb78dbca6e9d39a30f0c4b6cf282f656e60
GorbanArtemiy <artygor5@yandex.ru>
2024-01-07 21:17:38 +0300

    matrix var16

--- tasks/matrix/tasks/16/solution.h
+++ tasks/matrix/tasks/16/solution.h
@@ -2,10 +2,29 @@
 
 #include <stdint.h>
 #include <stdio.h>
-
-typedef int32_t** Matrix;
+#include <stdlib.h>
+#include <string.h>
 
 int Task() {
-    // write your solution here
+    char filename[50] = {""};
+    printf("file name: ");
+    scanf("%s", filename);
+    FILE *matrix_file = fopen(filename, "r");
+    char line[50] = {""};
+    int counter = 0;
+    char temp = 'a';
+    int length = 0;  // только работает если все числа меньше 10 и в конце строк нет пробелов
+    while (fgets(line, 50, matrix_file)) {
+        while (!(line[length] == line[length + 1])) {
+            length += 1;
+        }
+        length -= 1;
+        temp = line[2 * counter];
+        line[2 * counter] = line[length - (2 * counter + 1)];
+        line[length - (2 * counter + 1)] = temp;
+        counter += 1;
+        printf("%s", line);
+    }
+    fclose(matrix_file);
     return 0;
 }
